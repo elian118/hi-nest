ca83e4c0cf15b80d38a4aabd3285607222ee3e85
김광회 <postelian@gmail.com>
2022-08-05 12:57:37 +0900

    Add description of relationship between nest.js and express, fastify framework.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -71,3 +71,25 @@ export class MoviesController {
   "year": 2022
 }
 */
+
+/*
+nest 는 express 위에서 작동한다.
+그래서 아래와 같이 express 에 직접 접근하는 방식도 가능하다.
+
+@Get()
+  getAll(@Req req @Res res): Movie[] {
+    res.json()
+    return this.moviesService.getAll();
+  }
+
+그러나, 위 방식은 nest 에서 fastify 프레임워크에 접근하고자 할 때 문제가 된다.
+
+nest 는 express, fastify 두 개 프레임워크에서 동작하는데,
+만약 nest 에서 @Req @Res 를 사용해 express 방식을 적용할 경우,
+여기서 fastify 기능을 사용할 수 없기 때문이다.
+
+fastify 는 express 를 개선해 더 빠르게 성능을 개선한 프레임워크다.
+그래서 보통은 express 방식을 지양하고
+nest 에서 fastify 방식을 사용하는 걸 택한다.
+
+*/
