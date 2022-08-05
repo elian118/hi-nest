165a400785bd2e5f7002e6d00ac5fcee8fff90d6
김광회 <postelian@gmail.com>
2022-08-05 12:47:33 +0900

    Extract movie module from app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
-
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app/app.controller';
+// @Module 은 @Controller, @Service 및 참조 클래스(엔티티, DTO 포함)들을
+// 타입으로도 인식 가능하도록 해준다. -> d.ts 자동 생성(nest 의존성 자동 주입)
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
